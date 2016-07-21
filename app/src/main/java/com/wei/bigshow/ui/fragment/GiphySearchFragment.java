6621bdad4bb6a79cd8ca6b17cc12cfefff8b58b8
韦泽鹏 <weizepeng@baofeng.com>
2016-07-21 18:07:04 +0800

    动态Menu与SearchView

--- app/src/main/java/com/wei/bigshow/ui/fragment/GiphySearchFragment.java
+++ app/src/main/java/com/wei/bigshow/ui/fragment/GiphySearchFragment.java
@@ -1,15 +1,22 @@
 package com.wei.bigshow.ui.fragment;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.SearchView;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Toast;
 
 import com.wei.bigshow.R;
 import com.wei.bigshow.common.base.BaseRecyclerFragment;
@@ -26,7 +33,10 @@ import io.nlopez.smartadapters.SmartAdapter;
  * Zeus 跳转 图片搜索类
  * created by Adam Wei
  */
-public class GiphySearchFragment extends BaseRecyclerFragment{
+
+public class GiphySearchFragment extends BaseRecyclerFragment {
+
+    private SearchView mSearchView;
 
     public static GiphySearchFragment instance(Bundle bundle) {
         GiphySearchFragment fragment = new GiphySearchFragment();
@@ -45,15 +55,52 @@ public class GiphySearchFragment extends BaseRecyclerFragment{
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+
         initView();
         initData();
+    }
+
+    public void initSearchView(final Menu menu) {
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        if (searchItem != null) {
+            MenuItemCompat.setOnActionExpandListener(
+                    searchItem, new MenuItemCompat.OnActionExpandListener() {
+                        @Override
+                        public boolean onMenuItemActionExpand(MenuItem item) {
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onMenuItemActionCollapse(MenuItem item) {
+                            return true;
+                        }
+                    });
+            mSearchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+            mSearchView.setInputType(InputType.TYPE_TEXT_VARIATION_FILTER);
+            mSearchView.setImeOptions(EditorInfo.IME_ACTION_DONE | EditorInfo.IME_FLAG_NO_FULLSCREEN);
+            mSearchView.setQueryHint(getString(R.string.action_search));
+            mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+            mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return onQueryTextChange(query);
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    Toast.makeText(getActivity(), newText, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+        }
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_giphy_search, menu);
         super.onCreateOptionsMenu(menu, inflater);
-//        mListener.onInitSearchView(menu);
+        initSearchView(menu);
     }
 
     @Override
--- app/src/main/java/com/wei/bigshow/ui/fragment/ZeusFragment.java
+++ app/src/main/java/com/wei/bigshow/ui/fragment/ZeusFragment.java
@@ -1,8 +1,17 @@
 package com.wei.bigshow.ui.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.wei.bigshow.R;
 import com.wei.bigshow.common.base.BaseRecyclerFragment;
 import com.wei.bigshow.model.story.PlotMeta;
 import com.wei.bigshow.model.story.PlotOptions;
@@ -27,12 +36,24 @@ public class ZeusFragment extends BaseRecyclerFragment{
         return fragment;
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        return inflater.inflate(R.layout.common_list_normal, container, false);
+    }
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        initToolbar();
         initView();
         initData();
+    }
+
+    public void initToolbar() {
+        getActivity().supportInvalidateOptionsMenu();
     }
 
     private void initView() {
@@ -58,6 +79,30 @@ public class ZeusFragment extends BaseRecyclerFragment{
         adapter.setItems(itemList);
     }
 
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.clear();
+        MenuInflater inflater = getActivity().getMenuInflater();
+        inflater.inflate(R.menu.menu_zeus_save, menu);
+        super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_zeus_save, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.action_save) {
+            Toast.makeText(getContext(), "Save", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onMoreData() {
